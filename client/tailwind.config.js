e4e775ac788f78a56e2b1c758d3f21aeb75d259c
samstha07 <samshrestha85@gmail.com>
2021-12-27 01:19:27 +0545

    fetched all posts on homepage

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -27,5 +27,20 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        '.container': {
+          width: '100%',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          '@screen sm': {
+            maxWidth: '640px',
+          },
+          '@screen md': { maxWidth: '768px' },
+          '@screen lg': { maxWidth: '975px' },
+        },
+      });
+    },
+  ],
 };
