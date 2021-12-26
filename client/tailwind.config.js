3e0f70d3b214989f0b86628674fcd588ea6936ad
samstha07 <samshrestha85@gmail.com>
2021-12-27 01:20:29 +0545

    Revert "fetched all posts on homepage"
    This reverts commit e4e775ac788f78a56e2b1c758d3f21aeb75d259c.

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -27,20 +27,5 @@ module.exports = {
       },
     },
   },
-  plugins: [
-    function ({ addComponents }) {
-      addComponents({
-        '.container': {
-          width: '100%',
-          marginLeft: 'auto',
-          marginRight: 'auto',
-          '@screen sm': {
-            maxWidth: '640px',
-          },
-          '@screen md': { maxWidth: '768px' },
-          '@screen lg': { maxWidth: '975px' },
-        },
-      });
-    },
-  ],
+  plugins: [],
 };
